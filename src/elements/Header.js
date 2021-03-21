605dc1efc15f329c1114dfea46b00f8ebe0b7226
just13just <bkryzhniy@gmail.com>
2021-03-22 00:42:14 +0200

    feat: add counter to header element

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import FormElement from './FormElement'
 
 
-const Header = ({ countSaved }) => {
+const Header = () => {
+
+    const saved = useSelector(state => state.saved)
 
     return (
         <header className="header">
 
             <FormElement />
 
-            {countSaved &&
+            {saved.length > 0 &&
                 <div className="header__checked-row pt-3">
-                    Saved: {countSaved}
+                    Saved: {saved.length}
                 </div>}
         </header>
     )
